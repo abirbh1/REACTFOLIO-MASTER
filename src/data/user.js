eb865ed830feb7aa5f3dfc8c7c5fb2438ecdb4e9
abirbahri <abir.bahri.1@ulaval.ca>
2025-05-20 01:22:08 -0400

    first try

---
+++ src/data/user.js
@@ -0,0 +1,83 @@
+const INFO = {
+	main: {
+		title: "Abir Bahri",
+		name: "Abir Bahri",
+		email: "Abir.bahri.1@ulaval.ca",
+		logo: "../profile.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/abirbh1",
+		linkedin: "https://www.linkedin.com/in/abir-bahri-/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "I'm Abir Bahri.",
+		description:
+			"I am a PhD student at Université Laval, specializing in operations and decision systems with a focus on auction-based procurement in truckload transportation. With a background in computer science and intelligent systems, I apply artificial intelligence and optimization to address real-world logistics challenges. I am currently part of the CIRRELT research center, where I contribute to applied research in smart transportation and operational efficiency.",
+ },
+
+	about: {
+		title: "I’m Abir Bahri.",
+		description:
+			"My academic journey began in Tunisia, where I earned an engineering degree in computer science. Driven by a strong passion for intelligent systems, I also completed a research-based master’s degree in advanced systems at the same school. From my early research experiences, I became fascinated by the fields of recommender systems and customer profiling, which combine programming, data analysis, and the resolution of complex problems. I have always viewed Canada as a hub of research excellence, known for its high academic standards and collaborative opportunities. This aspiration led me to complete my master’s thesis internship in Canada, at the Université du Québec en Abitibi-Témiscamingue (UQAT). This enriching experience further strengthened my desire to pursue studies in a high-level research environment. My growing interest in operations and optimization led me to discover the Faculty of Business Administration (FSA) at Université Laval, renowned for its expertise in these areas—particularly through the CIRRELT research center. This innovative lab convinced me that FSA was the ideal place to sharpen my skills and achieve my goals. Moreover, my partner, who was already pursuing a master’s degree at FSA, recommended the institution to me, which further confirmed my decision. Since my admission, studying at FSA has allowed me to develop a broader and deeper understanding of decision-support systems, logistics, and modeling. These skills are central to my current research project, which focuses on optimizing bidding strategies in freight transportation. Working in such a stimulating environment has helped me strike a valuable balance between academic rigor and the practical application of knowledge. My studies at FSA ULaval will have a significant impact on both my academic and professional journey. They enable me to combine my expertise in artificial intelligence and optimization to tackle real-world challenges, while reinforcing my ambition to contribute to innovation in intelligent systems. Ultimately, I aim to grow into a leadership role where I can design innovative and strategic solutions for complex organizations."},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
